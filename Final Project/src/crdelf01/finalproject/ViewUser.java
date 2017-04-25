4e88dbd85272dfe1eb376daa991840ef049cf1a1
cdelfattore <cdelfattore@live.com>
2017-04-24 22:10:35 -0400

    still need to add view account info

--- Final Project/src/crdelf01/finalproject/ViewUser.java
+++ Final Project/src/crdelf01/finalproject/ViewUser.java
@@ -6,19 +6,18 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class LogOut
+ * Servlet implementation class ViewUser
  */
-@WebServlet("/LogOut")
-public class LogOut extends HttpServlet {
+@WebServlet("/ViewUser")
+public class ViewUser extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LogOut() {
+    public ViewUser() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -27,12 +26,8 @@ public class LogOut extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		HttpSession httpSes = request.getSession();
-		DatabaseAccess db = (DatabaseAccess) httpSes.getAttribute("dbInstance");
-		//db.disconnectFromDB();
-		
-		response.sendRedirect("login.jsp"); 
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
